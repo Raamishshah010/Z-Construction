1e10b00180d32075b9e8d6481a3fc37f96b7579a
Raamishshah010 <raamishshah010@gmail.com>
2022-10-23 23:04:52 +0500

    Home Page Almost Done

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// owl carousel init
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    dots: true,
+    stagePadding: -5,
+    responsiveBaseElement: 'body',
+    responsive: {
+        0: {
+            items: 1,
+        },
+        700: {
+            items: 1,
+        },
+        1220: {
+            items: 1,
+        },
+    },
+});
